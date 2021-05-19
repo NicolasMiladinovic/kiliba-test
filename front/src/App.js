9cd5d7f301ba2cfcec1c061bf72d563f0a1e977b
NicolasMiladinovic <nicolas.miladinovic@gmail.com>
2021-05-19 17:42:37 +0200

    Display user data

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,51 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+    };
+  }
+
+  getUsersData() {
+    fetch('/post/')
+      .then(res => {
+        return res.json();
+      })
+      .then(result => {
+        this.setState({
+          items: result
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
+  componentWillMount() {
+    this.getUsersData();
+  }
+
+  render() {
+    const { items } = this.state;
+
+    return (
+      <div>
+        <header className="App-header"><img src={logo} className="App-logo" alt="logo" /></header>
+
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              <p>email: {item.email} note: {item.note}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
